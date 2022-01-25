62c09b4b8cd7bbb7a9615f18b4b6659a6ca9e815
MUHAMMAD AKMAL <74851184+imbahere@users.noreply.github.com>
2022-01-25 17:33:29 +0800

    Update DNS.sh

--- DNS.sh
+++ DNS.sh
@@ -47,9 +47,37 @@ function installResolv() {
 	# Save DNS settings
 	echo CLIENT_DNS_1=${CLIENT_DNS_1}" >/etc/resolvconf/resolv.conf.d/head
 CLIENT_DNS_1=${CLIENT_DNS_1}" >/etc/resolv.conf
+}
 
-wget -q -O /usr/local/bin/menu 'https://raw.githubusercontent.com/imbahere/doctype/default/plugins/menu' && \
-chmod +x /usr/local/bin/menu
+function manageMenu() {
+	echo "Welcome to WireGuard-install!"
+	echo "The git repository is available at: https://github.com/imbahere/DNS"
+	echo ""
+	echo "It looks like DNS is already installed."
+	echo ""
+	echo "What do you want to do?"
+	echo "   1) Install Permanent DNS"
+	echo "   2) Revoke existing user"
+	echo "   3) Uninstall WireGuard"
+	echo "   4) Exit"
+	until [[ ${MENU_OPTION} =~ ^[1-4]$ ]]; do
+		read -rp "Select an option [1-4]: " MENU_OPTION
+	done
+	case "${MENU_OPTION}" in
+	1)
+		installResolv
+		;;
+	2)
+		revokeClient
+		;;
+	3)
+		uninstallWg
+		;;
+	4)
+		exit 0
+		;;
+	esac
+}
 
 # Check for root, virt, OS...
 initialCheck
